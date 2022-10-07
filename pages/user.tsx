78fbabb94f724a5a33d91e65a2fc99b42e50f363
Steve Moritani <steve.hayato.m@gmail.com>
2022-10-07 10:06:53 -0700

    user page filled

--- pages/user.tsx
+++ pages/user.tsx
@@ -4,9 +4,28 @@ import { getAuth } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 import ErrorMessage from "../components/ErrorMessage";
+import { doc } from "firebase/firestore";
+import { useDocument } from "react-firebase-hooks/firestore";
+import { db } from "../utils/initAuth";
+import { DB_USER_DATA, UNITS } from "../utils/staticValues";
 
 const user = () => {
+  const currUser =
+    getAuth().currentUser !== null ? getAuth().currentUser : { uid: "" };
+  const [userData, userDataLoading, userDataError] = useDocument(
+    doc(db, DB_USER_DATA, currUser!.uid),
+    {}
+  );
   const [user, loading, error] = useAuthState(getAuth());
+
+  const missedVocabData: ReactNode = UNITS.map((unit) => {
+    let missed = "-";
+    const unitName = "unit" + unit;
+    if (userData && userData.data()?.vocab.missedIds[unitName]) {
+      missed = userData.data()?.vocab.missedIds[unitName];
+    }
+    return <p key={unit}>{`Unit${unit} : ${missed}`}</p>;
+  });
 
   return (
     <Container>
@@ -28,8 +47,14 @@ const user = () => {
             </Col>
           </Row>
           <hr />
-          <h1>History</h1>
-          <Row></Row>
+          <h2>History</h2>
+          {userData && <p>{userData.data()?.history.type.unit[0]}</p>}
+          <h2>Vocabulary</h2>
+          <h5>お気に入り</h5>
+          <h5>間違えた単語</h5>
+          <Row>
+            <Col>{missedVocabData}</Col>
+          </Row>
         </>
       )}
     </Container>
